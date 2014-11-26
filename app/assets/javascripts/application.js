8202354ed3e7cabaccef79cd4804b52bf021678d
Alison Sinton <alsinton1@gmail.com>
2014-11-25 21:54:42 -0500

    added bootstrap and customizations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootsrap
 //= require jquery
 //= require jquery_ujs
+//= require bootsrap
 //= require turbolinks
 //= require_tree .
